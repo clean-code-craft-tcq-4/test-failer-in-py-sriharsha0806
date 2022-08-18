147f61ae1975417d53ac0efdce9b244570ab53e1
sriharsha annamaneni <sriharsha0806@gmail.com>
2022-08-18 12:01:09 +0530

    Update tshirts.py

--- tshirts.py
+++ tshirts.py
@@ -1,11 +1,13 @@
-
-def size(cms):
-    if cms < 38:
-        return 'S'
-    elif cms > 38 and cms < 42:
-        return 'M'
-    else:
-        return 'L'
+class tshirts:
+    def size(self, cms):
+        if cms < 38:
+            return 'S'
+        elif cms in range(38,43):
+            return 'M'
+        elif cms in range(43,50):
+            return 'L'
+        else:
+            return 'Custom Design is required'
 
 
 assert(size(37) == 'S')
