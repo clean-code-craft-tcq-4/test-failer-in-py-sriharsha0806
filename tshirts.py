f8777da371ffb741595052dbb19839da20009d02
sriharsha annamaneni <sriharsha0806@gmail.com>
2022-08-23 13:42:43 +0530

    Update tshirts.py

--- tshirts.py
+++ tshirts.py
@@ -11,4 +11,7 @@ class tshirts:
             return 'Custom Design is required'
 
 
-
+assert(tshirts.size(37) == 'S')
+assert(tshirts.size(40) == 'M')
+assert(tshirts.size(43) == 'L')
+print("All is well (maybe!)\n")
