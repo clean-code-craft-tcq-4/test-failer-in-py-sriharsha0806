bd2684ba8e1d89d74f7b5aed27959f9038687446
sriharsha annamaneni <sriharsha0806@gmail.com>
2022-08-23 17:43:16 +0530

    added open closed principle

--- tshirts.py
+++ tshirts.py
@@ -1,10 +1,33 @@
-class tshirts:    
+# Open-closed principle
+class tshirtsize:
+    @staticmethod
+    def size(cms):
+        pass
+
+class Small(tshirtsize):
     @staticmethod
     def size(cms):
         if cms < 38:
             return 'S'
-        elif cms in range(38,43):
-            return 'M'
-        else:
-            return 'L'
         
+        
+class Medium(tshirtsize):
+    @staticmethod
+    def size(cms):
+        if cms in range(38,43):
+            return 'M'
+        
+class Large(tshirtsize):
+    @staticmethod
+    def size(cms):
+        if cms >= 43:
+            return 'L'
+     
+sizes = [Small, Medium, Large]
+
+class tshirts:    
+    @staticmethod
+    def categorize(cms):
+        for size in sizes:
+            if size.size(cms):
+                return size.size(cms)
