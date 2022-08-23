f5ff778551bd10d6511aeb6495e511450b3358d6
sriharsha annamaneni <sriharsha0806@gmail.com>
2022-08-23 13:45:31 +0530

    Update tshirts.py

--- tshirts.py
+++ tshirts.py
@@ -5,13 +5,6 @@ class tshirts:
             return 'S'
         elif cms in range(38,43):
             return 'M'
-        elif cms in range(43,50):
-            return 'L'
         else:
-            return 'Custom Design is required'
-
-
-assert(tshirts.size(37) == 'S')
-assert(tshirts.size(40) == 'M')
-assert(tshirts.size(43) == 'L')
-print("All is well (maybe!)\n")
+            return 'L'
+        
