8f25234391a28eb9a14b697b1cd7fc228b6b588f
sriharsha annamaneni <sriharsha0806@gmail.com>
2022-08-25 14:07:35 +0530

    Revert "Feedback"

--- misaligned.py
+++ misaligned.py
@@ -1,26 +1,13 @@
-# Rather than functions improve it using decorators
 
-class colors:
-    def __init__(self):
-        self.major_colors = ["White", "Red", "Black", "Yellow", "Violet"]
-        self.minor_colors = ["Blue", "Orange", "Green", "Brown", "Slate"]
-        self.formatted_string = []
-        
-    def print_color_map(self):
-        for i, major in enumerate(self.major_colors):
-            for j, minor in enumerate(self.minor_colors):
-                temp = i*5+j
-                self.formatted_string.append(self.formatter(temp, major, minor))
-        return len(self.major_colors) * len(self.minor_colors), self.formatted_string
-    
-    def formatter(self, idx, Major, Minor):
-        formatted_string = f'{idx}|{Major}|{Minor}'
-        return formatted_string
-    
-    def print_on_console(self, string):
-        for i in string:
-            print(i, "\n")
+def print_color_map():
+    major_colors = ["White", "Red", "Black", "Yellow", "Violet"]
+    minor_colors = ["Blue", "Orange", "Green", "Brown", "Slate"]
+    for i, major in enumerate(major_colors):
+        for j, minor in enumerate(minor_colors):
+            print(f'{i * 5 + j} | {major} | {minor}')
+    return len(major_colors) * len(minor_colors)
 
-#result = print_color_map()
-#assert(result == 25)
-#print("All is well (maybe!)\n")
+
+result = print_color_map()
+assert(result == 25)
+print("All is well (maybe!)\n")
--- tshirts.py
+++ tshirts.py
@@ -1,10 +1,14 @@
-class tshirts:    
-    @staticmethod
-    def size(cms):
-        if cms < 38:
-            return 'S'
-        elif cms in range(38,43):
-            return 'M'
-        else:
-            return 'L'
-        
+
+def size(cms):
+    if cms < 38:
+        return 'S'
+    elif cms > 38 and cms < 42:
+        return 'M'
+    else:
+        return 'L'
+
+
+assert(size(37) == 'S')
+assert(size(40) == 'M')
+assert(size(43) == 'L')
+print("All is well (maybe!)\n")
